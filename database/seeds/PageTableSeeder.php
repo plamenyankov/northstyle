ceb838f9c4a0f2c2ee023273995c3db0457523d6
vagrant <vagrant@homestead>
2015-12-28 07:16:47 +0000

    commented dynamic route

--- database/seeds/PageTableSeeder.php
+++ database/seeds/PageTableSeeder.php
@@ -12,53 +12,53 @@ class PageTableSeeder extends Seeder
     public function run()
     {
         DB::table('pages')->truncate();
-//        DB::table('pages')->insert([
-//            [
-//            'title'=>'About',
-//            'uri'=>'about',
-//            'content'=>'This is about page',
-//                'parent_id'=>null,
-//                'lft'=>3,
-//                'rgt'=>8,
-//                'depth'=>0
-//        ],
-//            [
-//                'title'=>'Contact',
-//                'uri'=>'contact',
-//                'content'=>'This is contact page',
-//                'parent_id'=>1,
-//                'lft'=>4,
-//                'rgt'=>5,
-//                'depth'=>1
-//            ],
-//            [
-//                'title'=>'FAQ',
-//                'uri'=>'faq',
-//                'content'=>'This is about page',
-//                'parent_id'=>1,
-//                'lft'=>6,
-//                'rgt'=>7,
-//                'depth'=>1
-//            ],
-//            [
-//                'title'=>'Media',
-//                'uri'=>'media',
-//                'content'=>'This is about page',
-//                'parent_id'=>null,
-//                'lft'=>1,
-//                'rgt'=>2,
-//                'depth'=>0
-//            ],
-//            [
-//                'title'=>'Characters',
-//                'uri'=>'characters',
-//                'content'=>'This is about page',
-//                'parent_id'=>null,
-//                'lft'=>9,
-//                'rgt'=>10,
-//                'depth'=>0
-//            ]
-//            ]
-//            );
+        DB::table('pages')->insert([
+            [
+            'title'=>'About',
+            'uri'=>'about',
+            'content'=>'This is about page',
+                'parent_id'=>null,
+                'lft'=>3,
+                'rgt'=>8,
+                'depth'=>0
+        ],
+            [
+                'title'=>'Contact',
+                'uri'=>'contact',
+                'content'=>'This is contact page',
+                'parent_id'=>1,
+                'lft'=>4,
+                'rgt'=>5,
+                'depth'=>1
+            ],
+            [
+                'title'=>'FAQ',
+                'uri'=>'faq',
+                'content'=>'This is about page',
+                'parent_id'=>1,
+                'lft'=>6,
+                'rgt'=>7,
+                'depth'=>1
+            ],
+            [
+                'title'=>'Media',
+                'uri'=>'media',
+                'content'=>'This is about page',
+                'parent_id'=>null,
+                'lft'=>1,
+                'rgt'=>2,
+                'depth'=>0
+            ],
+            [
+                'title'=>'Characters',
+                'uri'=>'characters',
+                'content'=>'This is about page',
+                'parent_id'=>null,
+                'lft'=>9,
+                'rgt'=>10,
+                'depth'=>0
+            ]
+            ]
+            );
     }
 }
